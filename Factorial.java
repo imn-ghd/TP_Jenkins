2455535759fe4bc804d707a244fe2de03a776f2b
Imene <imene_ghoudane@ens.univ-artois.fr>
2026-02-10 13:42:48 +0100

    introduction d'un bug volontaire

--- Factorial.java
+++ Factorial.java
@@ -5,7 +5,7 @@ public class Factorial {
             throw new IllegalArgumentException("Le nombre doit être positif !");
         }
         if (n == 0 || n == 1) {
-            return 1;
+            return 0;
         }
         return n * factorial(n - 1);
     }
